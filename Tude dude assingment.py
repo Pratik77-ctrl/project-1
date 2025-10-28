20954ba6000771454c297a2c339d90ee2eb069f9
pratik  kumar patel <pra77tik@gmail.com>
2025-10-28 12:51:25 +0530

    Add files via upload

---
+++ Tude dude assingment.py
@@ -0,0 +1,34 @@
+# assingnment 1
+
+num1 = int(input("enter your number :"))
+num2 = int(input("enter your number :"))
+
+"""addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+"""
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print (addition)
+print (subtraction)
+print (multiplication)
+print (division)
+
+""" i have taken num1 = 2300
+and num2 = 690,
+addition = 2990
+subtraction = 1610
+multiplication = 15787000
+division = 3.33 """
+
+first_name = input("enter your name:")
+sur_name = input("enter your name:")
+full_name = first_name + ' ' + sur_name
+print(full_name) 
+""""first name = pratik kumar
+last name = patel
+concatinated name = pratik kumar patel"""
